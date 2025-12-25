530aa355b7e3a7605e32ec74b9f43fa9fb3a3e14
atom <sthaejan00@gmail.com>
2025-12-25 22:21:10 +0545

    :art: fic the Fodo service

--- src/Business/Business.Common/TenantDomain/IFoDoService.cs
+++ src/Business/Business.Common/TenantDomain/IFoDoService.cs
@@ -6,7 +6,7 @@ namespace Business.Common.TenantDomain;
 
 public interface IFodoService
 {
-    Task<Result<List<FodoResponseDto>>> GetFodosForAdminAsync(string? tenantId = null, CancellationToken cancellationToken = default);
+    Task<Result<List<FodoResponseDto>>> GetFodosForAdminAsync(CommonPaginationRequestModel requestModel, CancellationToken cancellationToken = default);
     Task<Result<FodoResponseDto>> GetFodoByIdAsync(string id, CancellationToken cancellationToken = default);
     Task<Result<FodoResponseDto>> CreateAsync(CreateFodoDto dto, CancellationToken cancellationToken = default);
     Task<Result<FodoResponseDto>> UpdateAsync(string id, UpdateFodoDto dto, CancellationToken cancellationToken = default);
